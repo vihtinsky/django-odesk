20b5271e9bf7e95a1f49a3494d4714599aad56fd
Volodymyr Hotsyk <gotsyk@gmail.com>
2010-10-21 00:27:14 +0300

    Use class-based views

--- django_odesk/auth/views.py
+++ django_odesk/auth/views.py
@@ -4,33 +4,40 @@ from django.contrib.auth import login, REDIRECT_FIELD_NAME
 from django_odesk.core.clients import DefaultClient
 from django_odesk.auth import ODESK_REDIRECT_SESSION_KEY, \
                               ODESK_TOKEN_SESSION_KEY
-
-def authenticate(request):
-    redirect_to = request.REQUEST.get(REDIRECT_FIELD_NAME, '')
-    request.session[ODESK_REDIRECT_SESSION_KEY] = redirect_to
-    odesk_client = DefaultClient()
-    return HttpResponseRedirect(odesk_client.auth.auth_url())
+from django.views.generic.base import RedirectView
 
 
-def callback(request, redirect_url=None):
-    odesk_client = DefaultClient()
-    frob = request.GET.get('frob', None)
-    if frob:
-        token, auth_user = odesk_client.auth.get_token(frob)
-        request.session[ODESK_TOKEN_SESSION_KEY] = token
-        #TODO: Get rid of (conceptually correct) additional request to odesk.com
-        user = django_authenticate(token = token)
-        if user:
-            login(request, user)
+class AuthenticateView(RedirectView):
+    
+    def get_redirect_url(self):
+        redirect_to = self.request.REQUEST.get(REDIRECT_FIELD_NAME, '')
+        self.request.session[ODESK_REDIRECT_SESSION_KEY] = redirect_to
+        odesk_client = DefaultClient()
+        return odesk_client.auth.auth_url()
+
+
+class CallBackView(RedirectView):
+    redirect_url = None
+    
+    def get_redirect_url(self):
+        odesk_client = DefaultClient()
+        frob = self.request.GET.get('frob', None)
+        if frob:
+            token, auth_user = odesk_client.auth.get_token(frob)
+            self.request.session[ODESK_TOKEN_SESSION_KEY] = token
+            #TODO: Get rid of (conceptually correct) additional request to odesk.com
+            user = django_authenticate(token = token)
+            if user:
+                login(self.request, user)
+            else:
+                pass 
+                #Probably the odesk auth backend is missing. Should we raise an error?
+            self.redirect_url = self.request.session.pop(ODESK_REDIRECT_SESSION_KEY, 
+                                               self.redirect_url)
+            if not self.redirect_url:
+                self.redirect_url =  '/'   
+            return self.redirect_url
+        
         else:
-            pass 
-            #Probably the odesk auth backend is missing. Should we raise an error?
-        redirect_url = request.session.pop(ODESK_REDIRECT_SESSION_KEY, 
-                                           redirect_url)
-        if not redirect_url:
-            redirect_url =  '/'   
-        return HttpResponseRedirect(redirect_url)
-    
-    else:
-        return HttpResponseRedirect(odesk_client.auth.auth_url())
-    
+            return odesk_client.auth.auth_url()
+        
